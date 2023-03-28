b5d08a5a19ff56a49763327b979f640ff9a88b23
kbecker13 <kaitlyn.c.becker@gmail.com>
2023-03-28 13:11:01 -0500

    Attempting with Ajax example instead

--- TAPLAP/fetchTest.py
+++ TAPLAP/fetchTest.py
@@ -1,12 +1,4 @@
 import requests #dependency
-########  imports  ##########
-from flask import Flask, jsonify, request, render_template
-app = Flask(__name__)
-#############################
-# Additional code goes here #
-#############################
-#########  run app  #########
-app.run(debug=True)
 
 url = "https://discordapp.com/api/webhooks/1090303823119130786/cWrwFU6xs77q8dnOsJEHwgxLw-Xn3Bq_bwgkWFujMGIaPo4y1Lf7zfom1W-mGB5-W-VS" #webhook url
 
@@ -34,14 +26,5 @@ except requests.exceptions.HTTPError as err:
 else:
     print("Payload delivered successfully, code {}.".format(result.status_code))
 
-@app.route('/')
-def home_page():
-    example_embed='This string is from python'
-    return render_template('index.html', embed=example_embed)
-@app.route('/test', methods=['GET', 'POST'])
-def testfn():
-    if request.method == 'POST':
-        print(request.get_json())
-        return 'Success',200
 #this is the webhook to my debug log
 #https://discordapp.com/api/webhooks/1090303823119130786/cWrwFU6xs77q8dnOsJEHwgxLw-Xn3Bq_bwgkWFujMGIaPo4y1Lf7zfom1W-mGB5-W-VS
